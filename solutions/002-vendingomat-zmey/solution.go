f83dc29678941b9d5c7cb15eebd5dae62e74fb85
Александр Заболотский <zabolotskiyav@sksr.ru>
2026-09-14 17:53:41 +0300

    002: finish flag unpack/pack solution

--- solutions/002-vendingomat-zmey/solution.go
+++ solutions/002-vendingomat-zmey/solution.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 	"strings"
 
 	"neon-grid/solutions/kit"
 )
+
+const (
+	SIZE_MASK  = 0b111111
+	FAST_MASK  = 0x40
+	IMPORT_BIT = 0x80
+)
+
+type Triplet struct {
+	Size, Fast, Imp uint64
+}
 
 // reads maximun lineCount lines from a text file, starting with startLine.
 // if startLine = 0, it means from the beginning, lines count from 0.
@@ -24,7 +34,12 @@ func readFromTextFile(path string, startLine, lineCount int) ([]string, error) {
 		startLine = 0
 	}
 
-	lines := make([]string, 0, lineCount)
+	var lines []string
+	if lineCount < 0 {
+		lines = make([]string, 0, 4)
+	} else {
+		lines = make([]string, 0, lineCount)
+	}
 	scanner := bufio.NewScanner(fd)
 	for scanner.Scan() {
 		if startLine > 0 {
@@ -72,21 +87,83 @@ func scanInputBytes() ([]byte, error) {
 	return bytes, nil
 }
 
+func scanTriplets() ([]Triplet, error) {
+	lines, err := readFromTextFile(kit.Data("002", "input.txt"), 1, -1)
+	if err != nil {
+		return nil, err
+	}
+
+	triplets := make([]Triplet, 0, len(lines))
+
+	for _, line := range lines {
+		parts := strings.Fields(line)
+		if len(parts) != 3 {
+			return nil, errors.New("data triplet is wrong - must be 3 parts")
+		}
+
+		size, err := strconv.ParseUint(parts[0], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing size column: %v", err)
+		}
+		fast, err := strconv.ParseUint(parts[1], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing fast column: %v", err)
+		}
+		imp, err := strconv.ParseUint(parts[2], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing imp column: %v", err)
+		}
+
+		triplets = append(triplets, Triplet{Size: size, Fast: fast, Imp: imp})
+	}
+
+	return triplets, nil
+}
+
 // Part1 декодирует байты из первой строки input.txt в строки вида
 // "SIZE=42 FAST=no IMPORT=yes".
 func Part1() string {
-	_, err := scanInputBytes()
+	bytes, err := scanInputBytes()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Sprintf("ERROR: %v", err)
 	}
 
-	return "foo"
+	var out strings.Builder
+
+	for _, b := range bytes {
+		vol := b & SIZE_MASK
+		fast := "no"
+		if b&FAST_MASK != 0 {
+			fast = "yes"
+		}
+		imp := "no"
+		if b&IMPORT_BIT != 0 {
+			imp = "yes"
+		}
+		fmt.Fprintf(&out, "SIZE=%d FAST=%s IMPORT=%s\n", vol, fast, imp)
+	}
+
+	return strings.TrimSpace(out.String())
 }
 
 // Part2 упаковывает тройки "SIZE FAST IMPORT" из input.txt в hex-байт
 // или возвращает SIZE_OVERFLOW.
 func Part2() string {
-	panic("TODO")
+	triplets, err := scanTriplets()
+	if err != nil {
+		return fmt.Sprintf("ERROR: %v", err)
+	}
+	var out strings.Builder
+
+	for _, triplet := range triplets {
+		if triplet.Size > SIZE_MASK || triplet.Fast > 1 || triplet.Imp > 1 {
+			fmt.Fprintf(&out, "SIZE_OVERFLOW\n")
+			continue
+		}
+		packed := (triplet.Imp << 7) + (triplet.Fast << 6) + triplet.Size
+		fmt.Fprintf(&out, "0x%X\n", packed)
+	}
+	return strings.TrimSpace(out.String())
 }
 
 func main() {
