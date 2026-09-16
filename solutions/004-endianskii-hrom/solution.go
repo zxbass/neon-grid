adeffb75dd04475ba4aec2be072d72eb69444e77
Александр Заболотский <zabolotskiyav@sksr.ru>
2026-09-16 12:02:09 +0300

    004: endian read and reversed ASCII signatures

--- solutions/004-endianskii-hrom/solution.go
+++ solutions/004-endianskii-hrom/solution.go
@@ -3,40 +3,25 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
-	"io"
 	"os"
 	"strings"
 
 	"neon-grid/solutions/kit"
 )
 
-func readNums() ([]byte, error) {
-	df, err := os.Open(kit.Data("004", "nums.bin"))
-	if err != nil {
-		return nil, err
-	}
-
-	bytes, err := io.ReadAll(df)
-	if err != nil {
-		return nil, err
-	}
-
-	return bytes, nil
-}
-
 // Part1 читает nums.bin как little-endian u32 и возвращает строки
 // "value=<n> BE=0x<hex>".
 func Part1() string {
-	bytes, err := readNums()
+	bytes, err := os.ReadFile(kit.Data("004", "nums.bin"))
 	if err != nil {
 		return fmt.Sprintf("ERROR: %v", err)
 	}
 
 	var sb strings.Builder
 
-	sizeIn32Bits := len(bytes) / 4
-	for i := range sizeIn32Bits {
-		num := binary.LittleEndian.Uint32(bytes[i*4:])
+	numCount := len(bytes) / 4
+	for i := range numCount {
+		num := binary.LittleEndian.Uint32(bytes[i*4 : i*4+4])
 		fmt.Fprintf(&sb, "value=%-5d BE=0x%08X\n", num, num)
 	}
 
@@ -46,7 +31,30 @@ func Part1() string {
 // Part2 возвращает развёрнутые ASCII-подписи узлов из nums.bin
 // (слова, где все 4 байта печатные).
 func Part2() string {
-	panic("TODO")
+	bytes, err := os.ReadFile(kit.Data("004", "nums.bin"))
+	if err != nil {
+		return fmt.Sprintf("ERROR: %v", err)
+	}
+
+	var sb strings.Builder
+
+	numCount := len(bytes) / 4
+outer:
+	for i := range numCount {
+		st := i * 4
+		for k := range 4 {
+			if bytes[st+k] < 0x20 || bytes[st+k] > 0x7e {
+				continue outer
+			}
+		}
+		sb.WriteByte(bytes[st+3])
+		sb.WriteByte(bytes[st+2])
+		sb.WriteByte(bytes[st+1])
+		sb.WriteByte(bytes[st])
+		sb.WriteByte('\n')
+	}
+
+	return strings.TrimSpace(sb.String())
 }
 
 func main() {
